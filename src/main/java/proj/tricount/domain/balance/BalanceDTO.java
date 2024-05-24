4f84906d7d8e98c04a93b458078578635b2a01c1
ByeongJun-Jang <qudwns8616@gmail.com>
2024-05-24 17:36:53 +0900

    [edit] rollback domain:

--- src/main/java/proj/tricount/domain/balance/BalanceDTO.java
+++ src/main/java/proj/tricount/domain/balance/BalanceDTO.java
@@ -1,17 +1,11 @@
 package proj.tricount.domain.balance;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
-@Entity
 public class BalanceDTO {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long balanceDTOid;
 
     private Long payerId;
--- src/main/java/proj/tricount/domain/settlement/SettlementParticipant.java
+++ src/main/java/proj/tricount/domain/settlement/SettlementParticipant.java
@@ -1,15 +1,9 @@
 package proj.tricount.domain.settlement;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
-@Entity
 @Data
 public class SettlementParticipant {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long participantId;
 
     private Long settlementId;
