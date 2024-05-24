b2a7d77bc36c3eaaa0e5638ea57d37d42f3e250f
ByeongJun-Jang <qudwns8616@gmail.com>
2024-05-24 14:05:48 +0900

    [edit] test code and gradle

--- src/test/java/proj/tricount/TricountApplicationTests.java
+++ src/test/java/proj/tricount/TricountApplicationTests.java
@@ -1,10 +1,7 @@
 package proj.tricount;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 public class TricountApplicationTests {
