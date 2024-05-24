c28aaa13788b4cc4d49f8b2f789d8a091f6cc5e8
ByeongJun-Jang <qudwns8616@gmail.com>
2024-05-24 15:22:36 +0900

    edit workflow and properties

--- src/test/java/proj/tricount/TricountApplicationTests.java
+++ src/test/java/proj/tricount/TricountApplicationTests.java
@@ -3,10 +3,12 @@ package proj.tricount;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("ci")
 public class TricountApplicationTests {
 
 	@Test
